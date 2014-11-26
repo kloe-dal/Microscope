1045d547dd680d791ed26f4207feb114a4f560b8
kloe-dal <kloe.dal@gmail.com>
2014-11-26 23:06:34 +0100

    Utiliser une Method pour envoyer le post

--- client/views/post/posts_submit.js
+++ client/views/post/posts_submit.js
@@ -6,9 +6,13 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val(),
       message: $(e.target).find('[name=message]').val()
-    };
+    }
+    
+    Meteor.call('post', post, function(error, id) {
+      if (error)
+        return alert(error.reason);
 
-    post._id = Posts.insert(post);
     Router.go('postPage', post);
+    });
   }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -1,6 +1,34 @@
 Posts = new Meteor.Collection('posts');
-Posts.allow ({
-  insert: function (userId, doc) {
-    return !!userId;
+
+Meteor.methods({
+  post: function(postAttributes) {
+    var user = Meteor.user(),
+      postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+    // assurons-nous que l'utilisateur est authentifié
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to post new stories");
+
+    // que le post a un titre
+    if (!postAttributes.title)
+      throw new Meteor.Error(422, 'Please fill in a headline');
+
+    // vérifions qu'il n'y pas d'autre post avec le même lien
+    if (postAttributes.url && postWithSameLink) {
+      throw new Meteor.Error(302, 
+        'This link has already been posted', 
+        postWithSameLink._id);
+    }
+
+    // filtrons pour prendre les attributs attendus
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      userId: user._id, 
+      author: user.username, 
+      submitted: new Date().getTime()
+    });
+
+    var postId = Posts.insert(post);
+
+    return postId;
   }
-})
+});
