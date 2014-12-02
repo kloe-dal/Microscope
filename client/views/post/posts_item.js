a029e6672538672e98e0449a4e21e89b68e150b7
kloe-dal <kloe.dal@gmail.com>
2014-12-02 11:19:04 +0100

    Griser lien vote quand non identifier ou déja voté

--- client/views/post/posts_item.js
+++ client/views/post/posts_item.js
@@ -8,9 +8,17 @@ Template.postItem.helpers({
     return a.hostname;
   },
   commentsCount: function () {    
-  return Comments.find({postId:this._id}).count();  }
+  return Comments.find({postId:this._id}).count();  },
+   upvotedClass: function() {
+     var userId = Meteor.userId();
+     if (userId && !_.include(this.upvoters, userId)) {
+       return 'btn-primary upvotable';
+     } else {
+       return 'disabled';
+     }
+   }
 });
 
 Template.postItem.events({
-  'click .upvote': function (e) {        e.preventDefault();    Meteor.call('upvote', this._id);
+  'click .upvotable': function (e) {        e.preventDefault();    Meteor.call('upvote', this._id);
 }});
